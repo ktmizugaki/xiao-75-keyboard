94719f6e39d9d238c123ee056ed75dd19ef201ba
mizugaki <k.t.mizugaki@gmail.com>
2023-07-08 11:49:44 +0900

    Adjust spacing of keymap

--- prog/keymap.c
+++ prog/keymap.c
@@ -54,7 +54,7 @@ enum ly_name {
 
 static const kbdkey_t s_key_matrix[MUM_LY][NUM_PINS][NUM_PINS-1] = {
     [LY_BASE] = LAYOUT(
-        KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_DEL,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_HOME,
          KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
           KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,   KC_ENT,        KC_PGDN,
@@ -63,7 +63,7 @@ static const kbdkey_t s_key_matrix[MUM_LY][NUM_PINS][NUM_PINS-1] = {
                                               KC_BTN1,   KC_BTN2,     AG_MS_X,AG_MS_Y
     ),
     [LY_FUNC] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+        _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_DEL,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,
          _______,  _______, _______, _______, CMD_BLR, _______, _______, _______, KC_INS,  _______, KC_PSCR, _______, _______, KC_INT3, KC_VOLU,
           _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU,   _______,       KC_VOLD,
